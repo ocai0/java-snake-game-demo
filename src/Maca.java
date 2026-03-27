709d44f2b6a03ea36fd08941c876b3b8557c97a0
Paulo Vítor <paulovitoralsp@yahoo.com.br>
2026-03-27 18:58:27 -0300

    Implementado crescimento da cobra ao consumir a fruta (Maca).

--- src/Maca.java
+++ src/Maca.java
@@ -6,6 +6,8 @@ public class Maca extends Fruta {
     
     @Override
     public void efeito(GamePanel game) {
+        game.bodyParts++;
+        game.applesEaten++;
         System.out.println("Maçã consumida: +1 no tamanho da cobra");
     }
 }
